20087ea63c068255965151258218abc61f50e5fd
Hassan, Md Sahil - (sahilhassan) <sahilhassan@email.arizona.edu>
2020-06-13 20:55:09 -0700

    H-matrix to vhdl conversion support added

---
+++ utilities/Hmat_to_interconnect_generator/HmatToInterconnect.c
@@ -0,0 +1,111 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main(int argc, char *argv[]){
+
+int N;  // = 1296;
+int M;  // = 648;
+int dv; // = 4;
+int dc; // = 8;
+int m,n,k;
+
+
+// Reading size of Hmat through provided file
+FILE *hmatsize;
+hmatsize = fopen(argv[1],"r");
+
+fscanf(hmatsize,"%d", &N);
+fscanf(hmatsize,"%d", &M);
+fscanf(hmatsize,"%d", &dv);
+fscanf(hmatsize,"%d", &dc);
+
+// Initializing Matrix data structures
+int **Dform, **DVform;
+Dform = (int **)calloc(M, sizeof(int *)); 
+for (m=0; m<M; m++) Dform[m] = (int *)calloc(dc, sizeof(int));
+DVform = (int **) calloc(N, sizeof(int *));
+for (n=0; n<N; n++) DVform[n] = (int *) calloc(dc, sizeof(int));
+
+// Initializing Matrix files
+FILE *DformFile, *DVformFile;
+DformFile = fopen(argv[2],"r"); //("IRISC_dv4_R050_L54_N1296_Dform", "r");
+DVformFile = fopen(argv[3],"r");  //("IRISC_dv4_R050_L54_N1296_DVform", "r");
+
+// Reading Matrix files into Matrix data structure
+for (m=0; m<M; m++){
+  for (k=0; k<dc; k++)
+    fscanf(DformFile, "%d", &Dform[m][k]);
+}
+
+for (n=0; n<N; n++){
+  for (k=0; k<dv; k++)
+    fscanf(DVformFile, "%d", &DVform[n][k]);
+}
+
+
+// Creating output file
+FILE *outputfile;
+outputfile = fopen(argv[4],"w");      //("output.vhd","w");
+
+
+// Generating VN_out_sig to CN_in_sig -- VNU connections serialized
+// Need to go over DVform matrix
+
+  // Initialize CNU connection counter array
+  int *CN_conn_count;
+  CN_conn_count = (int *)calloc(M, sizeof(int));
+
+  //for (m=0; m<M; m++) CN_conn_count[m] = 0; // Resetting CN_conn_count values to 0
+
+  for (n=0; n<N; n++){
+    for (k=0; k<dv; k++){
+      fprintf(outputfile, "CN_in_sig(%d)\t<=\tVN_out_sig(%d);\n", (DVform[n][k]*dc)+CN_conn_count[DVform[n][k]], (n*dv)+k);
+      CN_conn_count[DVform[n][k]] ++;
+    }
+  }
+
+  fprintf(outputfile, "\n\n");
+
+// Generating VN_out_sigd to CN_in_sigd -- VNU connections serialized and replicated between [0,1295]
+// Need to go over DVform matrix
+  // Initialize CNU connection counter array
+  /*int *CN_conn_count;
+  CN_conn_count = (int *)calloc(M, sizeof(int));*/
+  for (m=0; m<M; m++) CN_conn_count[m] = 0; // Resetting CN_conn_count values to 0
+
+  for (n=0; n<N; n++){
+    for (k=0; k<dv; k++){
+      fprintf(outputfile, "CN_in_sigd(%d)\t<=\tVN_out_sigd(%d);\n", (DVform[n][k]*dc)+CN_conn_count[DVform[n][k]], n);
+      CN_conn_count[DVform[n][k]] ++;
+    }
+  }
+  free(CN_conn_count);
+  fprintf(outputfile, "\n\n\n");
+
+
+// Generating VN_in_sig to CN_out_sig -- CNU signals are serialized, VNU matrix are decided based on connection
+// Need to go over Dform matrix
+
+  // Initialize VNU connection counter array
+  int *VN_conn_count;
+  VN_conn_count = (int *)calloc(N, sizeof(int));
+
+  for (m=0; m<M; m++){
+    for (k=0; k<dc; k++){
+      fprintf(outputfile, "VN_in_sig(%d)\t<=\tCN_out_sig(%d);\n", (Dform[m][k]*dv)+VN_conn_count[Dform[m][k]], (m*dc)+k);
+      VN_conn_count[Dform[m][k]]++;
+    }
+  }
+  fprintf(outputfile, "\n\n");
+  free(VN_conn_count);
+
+  free(Dform);
+  free(DVform);
+  fclose(DformFile);
+  fclose(DVformFile);
+  fclose(outputfile);
+  fclose(hmatsize);
+  return 0;
+}
---
+++ utilities/Hmat_to_interconnect_generator/Readme.md
@@ -0,0 +1,15 @@
+# H-matrix to VHDL interconnect conversion
+
+This folder contains a C program that converts any H-matrix for Regular Low Density Parity Check (LDPC) code into VHDL signal interconnect, in order to use with our high-throughput simulation testbed.
+
+##Instruction for execution
+1. Compile the HmatToInterconnect.c using the following command
+  `gcc -o hmattointerconnect HmatToInterconnect.c`
+2. Execute the generated binary file using the following command and arguments:
+  `./hmattointerconnect <File-size and degree of decoder> <File- H matrix from CNU perspective> <File- H matrix from VNU perspective> <File name- to write output in vhd>`
+
+  In this case, the execution command looks as-
+  `./hmattointerconnect IRISC_dv4_R050_L54_N1296_Dform_size IRISC_dv4_R050_L54_N1296_Dform IRISC_dv4_R050_L54_N1296_DVform OUTPUT.vhd`
+
+This should generate an output vhd file containing all signal connections according to the provided Regular H-Matrix in vhdl file format.
+This generated vhdl file can be used to replace the connections in 'PGaB.vhd' module.
